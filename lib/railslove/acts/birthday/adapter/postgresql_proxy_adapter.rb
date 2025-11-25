865715d2e83f0bf2652fee1d7a884c3d8d5a2325
Ahmed El-Masry <ahmed@joinhomebase.com>
2025-11-25 10:00:06 -0500

    EP-95: Add postgresql_proxy adapter support
    https://joinhomebase.atlassian.net/browse/EP-95
    
    We need to add support for the `postgresql_proxy` adapter in the `active_record_proxy_adapters` gem. This is necessary due to errors encountered during testing.
    
    While testing the `active_record_proxy_adapters` gem, two Sentry errors were reported. These errors were caused by a missing adapter in the `birthday` gem.
    
    * Implement support for the `postgresql_proxy` adapter in the `birthday` gem.
    * Ensure the adapter functions correctly as it did previously.
    
    * Sentry errors can be viewed [here](https://homebase.sentry.io/issues/7061098513/?environment=staging&project=28028&query=for_location&referrer=issue-stream).
    * The `birthday` gem can be found [here](https://github.com/pioneerworks/birthday).

---
+++ lib/railslove/acts/birthday/adapter/postgresql_proxy_adapter.rb
@@ -0,0 +1,11 @@
+# Birthday
+module Railslove
+  module Acts #:nodoc:
+    module Birthday #:nodoc:
+      module Adapter
+        class PostgreSQLProxyAdapter < PostgreSQLAdapter
+        end
+      end
+    end
+  end
+end
